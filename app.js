a6c2ed4a67e00f7dcc43657b00596ed95eecd697
Ionut Mocanu <ionutmocanu.djk@gmail.com>
2018-08-02 09:02:24 +0000

    Update to https

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var http = require('http');
+var fs = require('fs');
+var https = require('https');
 var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-var port = process.env.PORT || 3000;
+var privateKey  = fs.readFileSync('/etc/letsencrypt/live/ionutmocanu.com/privkey.pem', 'utf8');
+var certificate = fs.readFileSync('/etc/letsencrypt/live/ionutmocanu.com/cert.pem', 'utf8');
+var chain = fs.readFileSync('/etc/letsencrypt/live/ionutmocanu.com/chain.pem', 'utf8');
+
+var credentials = {key: privateKey, cert: certificate, ca: [chain]};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,7 +18,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 var routes = require("./routes/routes.js");
 app.get('/', routes.getData);
 
-// ======= START SERVER =======
-http.createServer(app).listen(port, function() {
-    console.log('Magic happening on port ' + port);
+// ======= START SERVER HHTP=======
+http.createServer(app).listen(3004, function() {
+    console.log('HTTP listening on port' + 3004);
+});
+
+// ======= START SERVER HHTPS=======
+https.createServer(credentials, app).listen(3005, function() {
+    console.log('HTTPS listening on port' + 3005);
 });
